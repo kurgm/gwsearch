edaadbe9f38e4251e6b7c05f5d01e47223464fa8
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-06-18 23:52:13 +0900

    Implement search app

---
+++ src/dag.js
@@ -0,0 +1,92 @@
+// @ts-check
+
+export class DAG {
+  /**
+   * @param {ReadonlyArray<string>} names
+   * @param {ReadonlyArray<ReadonlyArray<number>>} edges
+   */
+  constructor(names, edges) {
+    /** @readonly */
+    this.names = names;
+    /** @readonly */
+    this.edges = edges;
+
+    /** @type {Map<string, number>} */
+    const namesInv = new Map();
+    for (let i = 0; i < names.length; i++) {
+      namesInv.set(names[i], i);
+    }
+    /** @readonly @type {Map<string, number>} */
+    this.namesInv = namesInv;
+
+  }
+
+  /** @param {string} source */
+  get(source) {
+    const sourceNum = this.namesInv.get(source);
+    if (typeof sourceNum === 'undefined') {
+      return [];
+    }
+    return this.edges[sourceNum].map((num) => this.names[num]);
+  }
+
+  /** @param {string[]} vertexNames */
+  hcd(vertexNames) {
+    if (vertexNames.length === 0) {
+      return [];
+    }
+    const vertices = vertexNames.map((name) => this.namesInv.get(name));
+    if (vertices.some((vertex) => typeof vertex === 'undefined')) {
+      return [];
+    }
+
+    const visitedSets = vertices.map((vertex) => new Set([vertex]));
+    const queues = vertices.map((vertex) => [vertex]);
+
+    let queuesTotalSize = vertices.length;
+
+    for (let index = 0; queuesTotalSize > 0; index = (index + 1) % vertices.length) {
+      if (queues[index].length === 0) {
+        continue;
+      }
+      const source = queues[index].shift();
+      queuesTotalSize--;
+      if (visitedSets.every((visitedSet) => visitedSet.has(source))) {
+        continue;
+      }
+      for (const target of this.edges[source]) {
+        if (!visitedSets[index].has(target)) {
+          queues[index].push(target);
+          queuesTotalSize++;
+          visitedSets[index].add(target);
+        }
+      }
+    }
+
+    const minSizeSet = visitedSets.reduce((aSet, bSet) => aSet.size < bSet.size ? aSet : bSet);
+    const intersection = [...minSizeSet].filter((item) => visitedSets.every((set) => set.has(item)));
+
+    const result = new Set(intersection);
+    for (const source of intersection) {
+      for (const target of this.edges[source]) {
+        result.delete(target);
+      }
+    }
+    return [...result].map((vertex) => this.names[vertex]);
+  }
+
+  /** @param {string} text */
+  static load(text) {
+    const names = [];
+    const edges = [];
+    for (let lineRe = /^(\S+) (.*)$/gmu, m; (m = lineRe.exec(text)); ) {
+      const source = m[1];
+      const targetNums = m[2] ? m[2].split(",").map((s) => parseInt(s)) : [];
+
+      names.push(source);
+      edges.push(targetNums);
+    }
+    const dag = new DAG(names, edges);
+    return dag;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,164 @@
+// @ts-check
+
+import 'regenerator-runtime/runtime';
+
+import { DAG } from './dag';
+
+// @ts-ignore
+import graphURL from '../graph/dag.txt';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = /** @type {HTMLFormElement} */(document.getElementById('search_form'));
+  const input = /** @type {HTMLInputElement} */(document.getElementById('search_query'));
+  const resultDiv = /** @type {HTMLDivElement} */(document.getElementById('result'));
+
+  /** @param {string} queryStr */
+  function parseQuery(queryStr) {
+    const tokens = queryStr.match(/[a-z][a-z0-9_-]{4,}|&CDP-[\dA-F]{4};|\S/gu) || [];
+    return tokens.map((token) => {
+      if (token.startsWith('&CDP-')) {
+        return `abst:cdp-${token.substr('&CDP-'.length, 4).toLowerCase()}`;
+      }
+      if (token.length >= 5) {
+        return token;
+      }
+      let codepoint = token.codePointAt(0).toString(16);
+      if (codepoint.length < 4) {
+        codepoint = `000${codepoint}`.slice(-4);
+      }
+      return `abst:u${codepoint}`;
+    });
+  }
+
+  /**
+   * @typedef {object} Desc
+   * @property {string} name
+   * @property {string[]} children
+   */
+
+  const graphPromise = fetch(graphURL).then(async (response) => {
+    const text = await response.text();
+    return DAG.load(text);
+  });
+  /**
+   * @param {string[]} query
+   * @returns {Promise<Desc[]>}
+   */
+  async function processQuery(query) {
+    const graph = await graphPromise;
+    return graph.hcd(query).map((name) => ({
+      name,
+      children: graph.get(name),
+    }));
+  }
+  /**
+   * @param {string[]} items
+   * @returns {Promise<Desc[]>}
+   */
+  async function getChildren(items) {
+    const graph = await graphPromise;
+    return items.map((name) => ({
+      name,
+      children: graph.get(name),
+    }));
+  }
+
+  let currentQuery = null;
+  /** @param {string} queryStr */
+  function doSearch(queryStr) {
+    const query = parseQuery(queryStr);
+    currentQuery = query;
+    processQuery(query)
+      .then((result) => {
+        if (currentQuery !== query) {
+          return;
+        }
+        showResult(result);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const query = input.value;
+    doSearch(query);
+  });
+
+  /** @param {Desc[]} result */
+  function showResult(result) {
+    while (resultDiv.firstChild) {
+      resultDiv.removeChild(resultDiv.firstChild);
+    }
+    if (!result.length) {
+      resultDiv.appendChild(document.createTextNode('(該当結果なし)'));
+      return;
+    }
+    const resultUl = generateUl(result);
+    resultDiv.appendChild(resultUl);
+  }
+  /** @param {Desc[]} descs */
+  function generateUl(descs) {
+    const ul = document.createElement('ul');
+    for (const { name, children } of descs) {
+      const li = document.createElement('li');
+      if (children.length) {
+        li.dataset.children = children.join(',');
+        const btn = document.createElement('button');
+        btn.appendChild(document.createTextNode('+'));
+        li.appendChild(btn);
+      }
+      if (name.startsWith('abst:')) {
+        let text = name.substring('abst:'.length);
+        if (/^abst:cdp-[0-9a-f]{4}$/.test(name)) {
+          text = `&CDP-${name.substring('abst:cdp-'.length).toUpperCase()};`;
+        } else if (/^abst:u[0-9a-f]{4,}$/.test(name)) {
+          const codepointHex = name.substring('abst:u'.length);
+          const char = String.fromCodePoint(parseInt(codepointHex, 16));
+          text = `U+${codepointHex.toUpperCase()}: ${char}`
+        }
+        li.appendChild(document.createTextNode(text));
+      } else {
+        const link = document.createElement('a');
+        link.href = `https://glyphwiki.org/wiki/${name}`;
+        const img = document.createElement('img');
+        img.src = `https://glyphwiki.org/glyph/${name}.50px.png`;
+        img.alt = img.title = link.title = name;
+        img.className = 'thumb';
+        link.appendChild(img);
+        link.appendChild(document.createTextNode(name));
+        li.appendChild(link);
+      }
+      ul.appendChild(li);
+    }
+    return ul;
+  }
+
+  resultDiv.addEventListener('click', (evt) => {
+    if (!(
+      evt.target instanceof HTMLButtonElement &&
+      evt.target.parentNode instanceof HTMLLIElement &&
+      evt.target.parentNode.dataset.children
+    )) {
+      return;
+    }
+    const btn = evt.target;
+    const li = evt.target.parentNode;
+    if (li.lastChild instanceof HTMLUListElement) {
+      btn.textContent = '+';
+      li.removeChild(li.lastChild);
+      return;
+    }
+    btn.textContent = '...';
+    const items = li.dataset.children.split(',');
+    getChildren(items).then((children) => {
+      if (!resultDiv.contains(li)) {
+        return;
+      }
+      const ul = generateUl(children);
+      btn.textContent = '-';
+      li.appendChild(ul);
+    });
+  })
+});

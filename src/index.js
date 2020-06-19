9a24e7f6b0cfbd8972f9bb229ab59722606ef8b8
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-06-19 13:32:54 +0900

    Sort results

--- src/index.js
+++ src/index.js
@@ -46,9 +46,9 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   async function processQuery(query) {
     const graph = await graphPromise;
-    return graph.hcd(query).map((name) => ({
+    return graph.hcd(query).sort(nameCompar).map((name) => ({
       name,
-      children: graph.get(name),
+      children: graph.get(name).sort(nameCompar),
     }));
   }
   /**
@@ -59,8 +59,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const graph = await graphPromise;
     return items.map((name) => ({
       name,
-      children: graph.get(name),
+      children: graph.get(name).sort(nameCompar),
     }));
+  }
+
+  /**
+   * @param {string} a
+   * @param {string} b
+   */
+  function nameCompar(a, b) {
+    const ucsRe = /^(?:abst:)?u([0-9a-f]{4,})/;
+    const ma = ucsRe.exec(a);
+    const mb = ucsRe.exec(b);
+    const ownedRe = /_/;
+    const idsRe = /^u2ff[0-9ab]-/;
+    return (
+      (-a.startsWith('abst:') - -b.startsWith('abst:')) ||
+      (+ownedRe.test(a) - +ownedRe.test(b)) ||
+      (+idsRe.test(a) - +idsRe.test(b)) ||
+      (+!ma - +!mb) ||
+      (ma && mb && (parseInt(ma[1], 16) - parseInt(mb[1], 16))) ||
+      (a > b ? 1 : a < b ? -1 : 0)
+    );
   }
 
   let currentQuery = null;

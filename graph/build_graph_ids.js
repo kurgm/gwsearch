c51e6817a1a6c49c3ac18defcba92cac1f92fe38
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-04-30 00:24:43 +0900

    Use IDS data from BabelStone

--- graph/build_graph_ids.js
+++ graph/build_graph_ids.js
@@ -4,7 +4,7 @@ import { createReadStream } from 'fs';
 import { createInterface } from 'readline';
 import { parseArgs } from 'util';
 import { DAG } from './lib/dag.js';
-import { isIDC, parseIDS } from './lib/ids.js';
+import { isIDC, parseIDS, UNREPRESENTABLE } from './lib/ids.js';
 
 const { positionals } = parseArgs({
   strict: true,
@@ -18,10 +18,12 @@ const [srcpath, dstpath] = positionals;
 
 /** @param {string} text */
 function tokenize(text) {
-  const charRe = /[^&]|&CDP-[0-9A-F]{4};/gu;
+  if (!text) return [];
+  const charRe = /[^&{}]|\{\d{1,3}\}|&CDP-[0-9A-F]{4};/gu;
 
   const charsRe = new RegExp(`^(?:${charRe.source})+$`, 'u');
   if (!charsRe.test(text)) {
+    console.warn(`invalid text: ${text}`);
     return [];
   }
 
@@ -29,6 +31,9 @@ function tokenize(text) {
   for (const token of text.match(charRe)) {
     if (token.startsWith('&CDP-')) {
       result.push(`cdp-${token.substr('&CDP-'.length, 4).toLowerCase()}`);
+    } else if (token.startsWith('{')) {
+      const number = token.slice(1, -1).padStart(4, '0');
+      result.push(`_bs-uc-${number}`); // BabelStone Unencoded Component
     } else {
       const codepoint = token.codePointAt(0).toString(16).padStart(4, '0');
       result.push(`u${codepoint}`);
@@ -47,24 +52,43 @@ async function* readIdsFile(path) {
   });
 
   for await (const line of inputRL) {
-    if (line.startsWith('#') || line.startsWith(';;')) {
+    if (line.startsWith('#') || line.startsWith('\ufeff#')) {
+      if (!/^#\t\{\d+\}\t/.test(line)) continue;
+      // the description of Unencoded Components
+      const columns = line.split('\t');
+      if (columns.length !== 4) continue;
+      const [, targetStr, , idsStr] = columns;
+      if (!idsStr) continue;
+      const targets_ = tokenize(targetStr);
+      if (targets_.length !== 1) {
+        throw new Error(`invalid target: ${targetStr}`);
+      }
+      const [target] = targets_;
+      yield { target, idses: [tokenize(idsStr)] };
       continue;
     }
+
     const columns = line.split('\t');
     if (columns.length < 3) {
       continue;
     }
 
-    const [, targetStr, ...idsStrs] = columns;
+    const [, targetStr, ...rest] = columns;
     const targets_ = tokenize(targetStr);
     if (targets_.length !== 1) {
       console.warn(`invalid target: ${targetStr}`);
       continue;
     }
     const [target] = targets_;
-    const idses = idsStrs.map((idsStr) => {
-      const m = /^([^\[]+)\[[^\]]*\]$/.exec(idsStr);
-      return tokenize(m ? m[1] : idsStr);
+    const idses = rest.flatMap((column) => {
+      if (column.startsWith('*')) return []; // comment
+
+      const m = /^\^([^$()]+)\$(?:\([^\)]*\))?$/.exec(column);
+      if (!m) {
+        console.warn(`invalid IDS: ${column}`);
+        return [];
+      }
+      return [tokenize(m[1])];
     });
     yield { target, idses };
   }
@@ -93,27 +117,31 @@ for await (const { target, idses } of readIdsFile(srcpath)) {
       // subtraction
       // Prevent X → ㇯XY and Y → ㇯XY from appearing in the graph
       // since neither X nor Y is part of ㇯XY
-      const parsed = parseIDS(ids);
-      idsTokens = [parsed]
-        .map(
-          /** @returns {string | string[]} */
-          function rec(subtree) {
-            if (typeof subtree === 'string') {
-              return subtree;
+      try {
+        const parsed = parseIDS(ids);
+        idsTokens = [parsed]
+          .map(
+            /** @returns {string | string[]} */
+            function rec(subtree) {
+              if (typeof subtree === 'string') {
+                return subtree;
+              }
+              if (subtree[0] === 'u31ef') {
+                // Replace ㇯XY with ？
+                return UNREPRESENTABLE;
+              }
+              return subtree.map(rec).flat();
             }
-            if (subtree[0] === 'u31ef') {
-              // Replace ㇯XY with ？
-              return UNREPRESENTABLE;
-            }
-            return subtree.map(rec).flat();
-          }
-        )
-        .flat();
+          )
+          .flat();
+      } catch (e) {
+        console.warn('IDS error', target, ids);
+        continue;
+      }
     }
 
-    const specialDc = /^u(246[0-9a-f]|247[0-3]|ff1f)$/; // encircled numerics and wildcard
     const dcs = idsTokens.filter(
-      (idcOrDc) => !isIDC(idcOrDc) && !specialDc.test(idcOrDc)
+      (idcOrDc) => !isIDC(idcOrDc) && idcOrDc !== UNREPRESENTABLE
     );
 
     for (const dc of dcs) {

49a8b9b346c03b3050981b0817ec32f7b6760a67
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-05-06 19:48:34 +0900

    Skip merging user-owned glyph nodes with entity

--- graph/build_alias_dump.js
+++ graph/build_alias_dump.js
@@ -17,6 +17,12 @@ const [srcpath, dstpath] = positionals;
 const union = new Union();
 
 for await (const { name, data } of readDump(srcpath)) {
+  if (name.includes('_')) {
+    // Skip merging user-owned glyphs with its entity glyph.
+    // User-owned glyphs sometimes have unreliable or wrong related (kanrenji) data.
+    // Merging them with commonly used entity glyphs would cause unwanted search results.
+    continue;
+  }
   const match = /^99:0:0:0:0:200:200:([^@:$]+)$/.exec(data);
   if (!match) {
     continue;
